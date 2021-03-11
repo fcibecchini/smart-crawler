2e16dc611220b8b78eb02632a2cd0734f905dbd7
fcibecchini <fabio.cibecchini@gmail.com>
2021-03-11 23:15:18 +0000

    refactor: remove vms

--- testsite/scripts/bootstrap_augeas.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ ! -e /tmp/augeas-updated ]; then
-	sudo add-apt-repository ppa:raphink/augeas
-	sudo apt-get update
-  	sudo apt-get -y install python-software-properties augeas-tools libaugeas-ruby augeas-lenses
-  	touch /tmp/augeas-updated
-fi
